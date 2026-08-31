fca518aae6497a13d01db0d34ef09c723604dee2
yoam <johan.zuniga.rodriguez@est.una.ac.cr>
2026-08-31 16:46:11 -0600

    Feature of actividadesView

--- src/main/java/Interfaz/actividades/actividadesView.java
+++ src/main/java/Interfaz/actividades/actividadesView.java
@@ -1,4 +1,9 @@
 package Interfaz.actividades;
 
+import javax.swing.*;
+
 public class actividadesView {
+    private JTextField textField1;
+    private JButton button1;
+    private JTable table1;
 }
