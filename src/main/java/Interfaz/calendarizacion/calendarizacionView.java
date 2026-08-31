2036795bbae06add7c59dbd7ce39b82870bef84c
yoam <johan.zuniga.rodriguez@est.una.ac.cr>
2026-08-31 17:56:57 -0600

    Feature Calendarizacion using another form in base

--- src/main/java/Interfaz/calendarizacion/calendarizacionView.java
+++ src/main/java/Interfaz/calendarizacion/calendarizacionView.java
@@ -1,4 +1,10 @@
 package Interfaz.calendarizacion;
 
+import javax.swing.*;
+
 public class calendarizacionView {
+    private JTextField textField1;
+    private JButton button1;
+    private JTable table1;
+    private JComboBox comboBox1;
 }
