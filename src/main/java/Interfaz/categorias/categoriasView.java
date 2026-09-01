b4e5baaca1746f8a0a339cbcc5634d880e7af174
DannyP <danny.perez.zamora@est.una.ac.cr>
2026-09-01 01:23:04 -0600

    Implementa view.form de estadíticas y categorías

--- src/main/java/Interfaz/categorias/categoriasView.java
+++ src/main/java/Interfaz/categorias/categoriasView.java
@@ -1,4 +1,16 @@
 package Interfaz.categorias;
 
+import javax.swing.*;
+
 public class categoriasView {
+    private JPanel panel1;
+    private JTable table1;
+    private JTextField textField1;
+    private JButton buscarButton;
+    private JButton imprimirButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton guardarButton;
+    private JButton borrarButton;
+    private JButton limpiarButton;
 }
--- src/main/java/Interfaz/estadisticas/estadisticasView.java
+++ src/main/java/Interfaz/estadisticas/estadisticasView.java
@@ -1,4 +1,13 @@
 package Interfaz.estadisticas;
 
+import javax.swing.*;
+
 public class estadisticasView {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton button1;
+    private JTextField textField2;
+    private JButton button2;
+    private JButton cargarButton;
+    private JTable table1;
 }
